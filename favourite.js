0d0b7d5ec4bb3a7e703f90ee8856d405615c38c0
NataliiaKostiuk <128825690+NataliiaKostiuk@users.noreply.github.com>
2024-09-26 20:27:19 +0100

    weather

--- favourite.js
+++ favourite.js
@@ -2,33 +2,42 @@
 const PRODUCT_LS_KEY = 'checkout';
 
 const favouriteList = document.querySelector('.favourite-list');
-console.log(favouriteList);
 const favouriteArr = JSON.parse(localStorage.getItem(PRODUCT_LS_KEY));
- console.log(favouriteArr);
 
  export function createMarkup(data) {
-    return data.map(({ name, avatar, id, link, createdAt }) => {
-      return `<li class="rout-list" data-id="${id}">
-        <img class="rout-img" src="${avatar}" alt="${id}">
-        <h3 class="rout-item">${name}</h3>
-        <p class="text">${createdAt}</p>
-        <a class="rout-link" href="${link}" class="link-rout">Oficial link</a>
-         <svg class="svg-heart-red" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="25" height="23">
-   <path d="M462.3 62.7C407.5 15.9 326.8 24 275.4 79.5L256 99.1L236.6 79.5C185.2 24 104.5 15.9 49.7 62.7C-16.6 122.4-16.6 227.5 49.7 287.3L233.2 467.5C240.6 474.6 248.3 480 256 480C263.7 480 271.4 474.6 278.8 467.5L462.3 287.3C528.6 227.5 528.6 122.4 462.3 62.7zM256 434.6L72.6 255.3C24.5 209.9 24.5 140.1 72.6 94.7C118.9 52.3 185.6 58.1 231.4 107.7L256 133.6L280.6 107.7C326.4 58.1 393.1 52.3 439.4 94.7C487.5 140.1 487.5 209.9 439.4 255.3L256 434.6z"/>
- </svg>
-      </li>`;
-    }).join('');
+    return data.map(({ name, avatar, id, link, createdAt,weather }) => {
+ const { avgtemp_c, condition: { icon, text } } = weather || {}; 
+
+    return `<li class="rout-list" data-id="${id}">
+      <a href="path-to-large-image1.jpg">
+        <img class="rout-img" src="${avatar}" alt="${id}"/>
+      </a>
+      <h3 class="rout-item">${name}</h3>
+      <p class="text">${createdAt}</p>
+      <div class="wrap-weather"
+      <a class="rout-link" href="${link}" class="link-rout">Official link</a>
+      <div class="forecast">
+        ${weather ? `
+          <img src="${icon}" alt="Weather icon" />
+          <p class="degree"> ${avgtemp_c} °C</p>
+        ` : '<p>We are working with it!!</p>'}
+      </div>
+      </div>
+      <svg class="svg-heart-red" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="25" height="23">
+        <path d="M462.3 62.7C407.5 15.9 326.8 24 275.4 79.5L256 99.1L236.6 79.5C185.2 24 104.5 15.9 49.7 62.7C-16.6 122.4-16.6 227.5 49.7 287.3L233.2 467.5C240.6 474.6 248.3 480 256 480C263.7 480 271.4 474.6 278.8 467.5L462.3 287.3C528.6 227.5 528.6 122.4 462.3 62.7zM256 434.6L72.6 255.3C24.5 209.9 24.5 140.1 72.6 94.7C118.9 52.3 185.6 58.1 231.4 107.7L256 133.6L280.6 107.7C326.4 58.1 393.1 52.3 439.4 94.7C487.5 140.1 487.5 209.9 439.4 255.3L256 434.6z"/>
+      </svg>
+    </li>`;
+  }).join('');
 }
 
 if (favouriteArr.length > 0) {
   favouriteList.insertAdjacentHTML('beforeend', createMarkup(favouriteArr));
 }
 else {
-    favouriteList.insertAdjacentHTML('beforeend', "You haven't chosen  any travalling yet.. "); 
+    favouriteList.insertAdjacentHTML('beforeend', "You haven't chosen  any travalling yet..  Push ♥ "); 
 }
 
 const favourItems = document.querySelector('.rout-list');
-console.log(favourItems);
 
 favouriteList.addEventListener('click', hendlerClickRemove);
 
--- index.js
+++ index.js
@@ -4,39 +4,74 @@ input.addEventListener('input', handelSearch);
 
 
 let globalData = []; 
- fetch("https://66eed6173ed5bb4d0bf21c25.mockapi.io/canada")
-    .then((resp) => {
-      console.log(resp);
-      if (!resp.ok) {
-        throw new Error(resp.statusText || "Примисово прокидаємо в catch");
-      }
-      return resp.json();
-    })
-    .then((data) => {
-      globalData = data;
-      container.insertAdjacentHTML('beforeend', createMarkup(data));
-       renderSavedHearts(); 
-      const heard = document.querySelector('.svg-heart');
-      container.addEventListener('click', hendlerClick);
-    })
-    .catch((err) => console.log(err));
 
+async function fetchData() {
+  try {
+    const resp = await fetch("https://66eed6173ed5bb4d0bf21c25.mockapi.io/canada");
+    if (!resp.ok) {
+      throw new Error(resp.statusText || "Error");
+    }
+    const data = await resp.json();
+    const forecasts = await Promise.all(data.map(async (obj) => {
+      const weather = await serviceSearchWearter(obj.location);
+      return { ...obj, weather }; 
+    }));
+     globalData = forecasts;
 
- function createMarkup(globalData) {
-    return globalData.map(({ name, avatar, id, link, createdAt }) => {
-      return `<li class="rout-list" data-id="${id}">
+    container.insertAdjacentHTML('beforeend', createMarkup(forecasts)); 
+    renderSavedHearts();
+    container.addEventListener('click', hendlerClick);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+fetchData();
+
+ async function serviceSearchWearter(city) {
+  try {
+    const BASE_URL = "http://api.weatherapi.com/v1";
+    const API_KEY = "bd3aded53f7b40c49d5231513232108";
+    const resp = await fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&lang=en`);
+
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+
+    const data = await resp.json();
+    return data.forecast.forecastday[0].day;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function createMarkup(globalData) {
+  return globalData.map(({ name, avatar, id, link, createdAt, weather }) => {
+    const { avgtemp_c, condition: { icon, text } } = weather || {}; 
+
+    return `<li class="rout-list" data-id="${id}">
       <a href="path-to-large-image1.jpg">
         <img class="rout-img" src="${avatar}" alt="${id}"/>
       </a>
-        <h3 class="rout-item">${name}</h3>
-        <p class="text">${createdAt}</p>
-        <a class="rout-link" href="${link}" class="link-rout">Oficial link</a>
-         <svg class="svg-heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="25" height="23">
-       <path d="M462.3 62.7C407.5 15.9 326.8 24 275.4 79.5L256 99.1L236.6 79.5C185.2 24 104.5 15.9 49.7 62.7C-16.6 122.4-16.6 227.5 49.7 287.3L233.2 467.5C240.6 474.6 248.3 480 256 480C263.7 480 271.4 474.6 278.8 467.5L462.3 287.3C528.6 227.5 528.6 122.4 462.3 62.7zM256 434.6L72.6 255.3C24.5 209.9 24.5 140.1 72.6 94.7C118.9 52.3 185.6 58.1 231.4 107.7L256 133.6L280.6 107.7C326.4 58.1 393.1 52.3 439.4 94.7C487.5 140.1 487.5 209.9 439.4 255.3L256 434.6z"/>
-       </svg>
-      </li>`;
-    }).join('');
-  }
+      <h3 class="rout-item">${name}</h3>
+      <p class="text">${createdAt}</p>
+      <div class="wrap-weather"
+      <a class="rout-link" href="${link}" class="link-rout">Official link</a>
+      <div class="forecast">
+        ${weather ? `
+          <img src="${icon}" alt="Weather icon" />
+          <p class="degree"> ${avgtemp_c} °C</p>
+        ` : '<p>We are working with it!!</p>'}
+      </div>
+      </div>
+      <svg class="svg-heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="25" height="23">
+        <path d="M462.3 62.7C407.5 15.9 326.8 24 275.4 79.5L256 99.1L236.6 79.5C185.2 24 104.5 15.9 49.7 62.7C-16.6 122.4-16.6 227.5 49.7 287.3L233.2 467.5C240.6 474.6 248.3 480 256 480C263.7 480 271.4 474.6 278.8 467.5L462.3 287.3C528.6 227.5 528.6 122.4 462.3 62.7zM256 434.6L72.6 255.3C24.5 209.9 24.5 140.1 72.6 94.7C118.9 52.3 185.6 58.1 231.4 107.7L256 133.6L280.6 107.7C326.4 58.1 393.1 52.3 439.4 94.7C487.5 140.1 487.5 209.9 439.4 255.3L256 434.6z"/>
+      </svg>
+    </li>`;
+  }).join('');
+}
+
+
 
 function handelSearch() {
   const searchValue = input.value.toLowerCase(); 
